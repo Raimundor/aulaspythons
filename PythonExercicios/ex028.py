ebf93cc4c6de480ab8417c25574c19019ebf5318
Raimundor <raimundorlima@gmail.com>
2019-09-26 21:29:36 -0300

    condições

--- PythonExercicios/ex028.py
+++ PythonExercicios/ex028.py
@@ -2,7 +2,7 @@ from random import randint
 from time import sleep
 comp = randint(0, 5) # faz o computador 'pensar'
 print('-=-'* 20)
-print('Vou pensar em número entre 0 e 5. Tente adivinhar...')
+print('Vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('-=-'* 20)
 num = int(input('Em que número eu pensei? ')) # jogador tenta adivinhar
 print('PROCESSANDO....')
